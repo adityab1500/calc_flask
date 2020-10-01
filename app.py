78c9063a735800438540b5871537e0cd053faf89
shubham000231 <48961698+shubham000231@users.noreply.github.com>
2020-10-01 14:29:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def add():
     return render_template('add.html')
 
-
+# this is for nothing
 @app.route("/result", methods=['POST', 'GET'])
 def calc():
     if request.method == 'POST':
@@ -15,9 +15,9 @@ def calc():
         a = int(result['a'])
         b = int(result['b'])
         s = result['op']
-
+        # this is for nothing
         res="hey"
-
+        # this is for nothing
         if s == "+":
             res = a + b
         if s == "-":
@@ -26,9 +26,9 @@ def calc():
             res = a * b
         if s == "/":
             res = a / b
-
+        # this is for nothing
         return render_template("result.html", res=res)
 
-
+# this is for nothing
 if __name__ == '__main__':
     app.run(debug=True)
